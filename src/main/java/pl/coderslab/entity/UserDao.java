63bbe597aba0a1cb7a33a9a706fa4e72a35beb67
piotr <mr.piotr.ratajczyk@gmail.com>
2021-05-28 10:49:20 +0200

    Adding findAll method

--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -4,6 +4,7 @@ import org.mindrot.jbcrypt.BCrypt;
 import pl.coderslab.DbUtil;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class UserDao {
 
@@ -11,7 +12,9 @@ public class UserDao {
 
     private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES(?, ?, ?)";
     private static final String FIND_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+    private static final String FIND_ALL_USER_QUERY = "SELECT * FROM users";
     private static final String UPDATE_USER_QUERY = "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
 
     public User create(User user) {
         try (Connection connection = DbUtil.connect(DB_NAME);
@@ -64,5 +67,37 @@ public class UserDao {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+    }
+
+
+    public User[] findAll() {
+        User[] users = new User[0];
+        try (Connection connection = DbUtil.connect(DB_NAME)) {
+            PreparedStatement prepStmt = connection.prepareStatement(FIND_ALL_USER_QUERY);
+            ResultSet resultSet = prepStmt.executeQuery();
+            while (resultSet.next()) {
+                users = Arrays.copyOf(users, users.length + 1);
+                int identify = resultSet.getInt("id");
+                String email = resultSet.getString("email");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                users[users.length - 1] = new User(identify, email, username, password);
+            }
+            return users;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
+
+    public void delete(int id) {
+        try (Connection connection = DbUtil.connect(DB_NAME)) {
+            PreparedStatement prepStmt = connection.prepareStatement(DELETE_USER_QUERY);
+            prepStmt.setInt(1, id);
+            prepStmt.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 }
--- src/test/java/pl/coderslab/Main01.java
+++ src/test/java/pl/coderslab/Main01.java
@@ -7,33 +7,43 @@ import pl.coderslab.entity.UserDao;
 public class Main01 {
     public static void main(String[] args) {
         UserDao userDao = new UserDao();
-        User user = new User();
-        user.setUserName("Arek1");
-        user.setEmail("a1rkadiusz.jozwiak@coderslab.pl");
-        user.setPassword("Pass");
-        userDao.create(user);
+//        User user = new User();
+//        user.setUserName("Arek1");
+//        user.setEmail("a1rkadiusz.jozwiak@coderslab.pl");
+//        user.setPassword("Pass");
+//        userDao.create(user);
+////
+////        System.out.println(user);
 //
-//        System.out.println(user);
+//        User user1 = userDao.read(3);
+//        System.out.println(user1);
+//
+//        User user2 = userDao.read(1);
+//        System.out.println(user2);
+//
+//        System.out.println();
+//        System.out.println();
+//
+//        User userToUpdate = userDao.read(1);
+//        System.out.println(userToUpdate);
+//        userToUpdate.setUserName("Arek");
+//        userToUpdate.setEmail("arkadiusz.jozwiak@coderslab.pl");
+//        userToUpdate.setPassword("Nowe");
+//        userDao.update(userToUpdate);
+//        User user3= userDao.read(1);
+//        System.out.println(user3);
 
-        User user1 = userDao.read(3);
-        System.out.println(user1);
+        User[] all = userDao.findAll();
+        for (User element: all) {
+            System.out.println(element);
+        }
 
-        User user2 = userDao.read(1);
-        System.out.println(user2);
+        userDao.delete(1);
 
-        System.out.println();
-        System.out.println();
-
-        User userToUpdate = userDao.read(1);
-        System.out.println(userToUpdate);
-        userToUpdate.setUserName("Arek");
-        userToUpdate.setEmail("arkadiusz.jozwiak@coderslab.pl");
-        userToUpdate.setPassword("Nowe");
-        userDao.update(userToUpdate);
-        User user3= userDao.read(1);
-        System.out.println(user3);
-
-
+        User[] all2 = userDao.findAll();
+        for (User element: all2) {
+            System.out.println(element);
+        }
 
     }
 }
